0b912f2b191c6611cc16371e63ab7e8795a294f8
Javed Bagwan <javed460@gmail.com>
2024-03-07 09:18:23 +0530

    Initial project structure

---
+++ src/main/java/com/example/fiverrlevelsengine/FiverrLevelsEngineApp.java
@@ -0,0 +1,13 @@
+package com.example.fiverrlevelsengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiverrLevelsEngineApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiverrLevelsEngineApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fiverrlevelsengine/FiverrLevelsEngineAppTests.java
@@ -0,0 +1,13 @@
+package com.example.fiverrlevelsengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiverrLevelsEngineAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
