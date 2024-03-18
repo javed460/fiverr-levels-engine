ca77f2e3f326d8715927b35d57e242e05f89cc2c
Javed Bagwan <javed460@gmail.com>
2024-03-18 09:06:45 +0530

    implemented findLevel endpoint

--- src/main/java/com/example/fiverrlevelsengine/controller/LevelFinderController.java
+++ src/main/java/com/example/fiverrlevelsengine/controller/LevelFinderController.java
@@ -1,16 +1,27 @@
 package com.example.fiverrlevelsengine.controller;
 
+import com.example.fiverrlevelsengine.domain.Parameters;
+import com.example.fiverrlevelsengine.service.LevelsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class LevelFinderController {
+    private final LevelsService levelsService;
 
     @GetMapping("/showFormLevelFinder")
     public String viewLevelFinderForm(Model model) {
         return "levelfinder";
+    }
+
+    @PostMapping("/findLevel")
+    public String processParameters(@ModelAttribute Parameters parameters, Model model) {
+        String level = levelsService.findLevel(parameters);
+        return level;
     }
 }
