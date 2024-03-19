36303250610de5d91225e25985fdcd9fe864d5f3
Javed Bagwan <javed460@gmail.com>
2024-03-19 09:05:31 +0530

    Added Parameters class

---
+++ src/main/java/com/example/fiverrlevelsengine/domain/Parameters.java
@@ -0,0 +1,13 @@
+package com.example.fiverrlevelsengine.domain;
+
+import lombok.Data;
+
+@Data
+public class Parameters {
+    private Integer successScore;
+    private Double rating;
+    private Integer responseRate;
+    private Integer orders;
+    private Integer uniqueClients;
+    private Integer earnings;
+}
