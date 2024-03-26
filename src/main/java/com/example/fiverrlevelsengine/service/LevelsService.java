41ad91fe0fb4cc318b8d87e6ab8d6752ef6b8bb3
Javed Bagwan <javed460@gmail.com>
2024-03-26 09:20:17 +0530

    updated levelservice

--- src/main/java/com/example/fiverrlevelsengine/service/LevelsService.java
+++ src/main/java/com/example/fiverrlevelsengine/service/LevelsService.java
@@ -2,19 +2,27 @@ package com.example.fiverrlevelsengine.service;
 
 import com.example.fiverrlevelsengine.domain.Levels;
 import com.example.fiverrlevelsengine.domain.Parameters;
+import com.example.fiverrlevelsengine.leveldeterminer.LevelDeterminer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LevelsService {
     private final Levels levels;
+    private final List<LevelDeterminer> levelDeterminers;
 
     public Levels getAllLevels() {
         return levels;
     }
 
-    public String findLevel(Parameters parameters) {
-        return null;
+    public String findLevel(Parameters inParameters) {
+        return levelDeterminers.stream()
+                .filter(levelDeterminer -> levelDeterminer.levelMet(inParameters))
+                .map(LevelDeterminer::getLevelName)
+                .findFirst()
+                .orElse("New Seller");
     }
 }
